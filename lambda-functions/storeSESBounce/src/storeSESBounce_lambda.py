91b69669474e2f64116ef50aa3c4688355dcf06d
Dave North <dnorth@signiant.com>
2017-10-23 09:56:46 -0400

    Add an expiry which we can use for TTL expiry

--- lambda-functions/storeSESBounce/src/storeSESBounce_lambda.py
+++ lambda-functions/storeSESBounce/src/storeSESBounce_lambda.py
@@ -2,6 +2,8 @@ import boto3
 import json
 import time
 import dateutil.parser
+import datetime
+import calendar
 
 # Helper class to convert a DynamoDB item to JSON.
 class DecimalEncoder(json.JSONEncoder):
@@ -15,48 +17,48 @@ class DecimalEncoder(json.JSONEncoder):
 
 def lambda_handler(event, context):
     #print("Received event: " + json.dumps(event, indent=2))
-    
+
     processed = False
     DYNAMODB_TABLE = "DEVOPS_SES_BOUNCES"
-    
+
     DDBtable = boto3.resource('dynamodb').Table(DYNAMODB_TABLE)
-    
+
     # Generic SNS headers
     SnsMessageId = event['Records'][0]['Sns']['MessageId']
     SnsPublishTime = event['Records'][0]['Sns']['Timestamp']
     SnsTopicArn = event['Records'][0]['Sns']['TopicArn']
     SnsMessage = event['Records'][0]['Sns']['Message']
-    
+
     print("Read SNS Message with ID " + SnsMessageId + " published at " + SnsPublishTime)
-    
+
     now = time.strftime("%c")
     LambdaReceiveTime = now;
-    
+
     # SES specific fields
     SESjson = json.loads(SnsMessage)
     sesNotificationType = SESjson['notificationType']
     sesMessageId = SESjson['mail']['messageId']
     sesTimestamp = SESjson['mail']['timestamp'] #the time the original message was sent
     sender = SESjson['mail']['source']
-    
+
     print("Processing an SES " + sesNotificationType + " with mID " + sesMessageId )
-    
+
     if (sesNotificationType == "Bounce"):
         print "Processing SES bounce messsage"
-        
+
         try:
             reportingMTA = SESjson['bounce']['reportingMTA']
         except:
             print "No reportingMTA provided in bounce notification"
             print("Received event: " + json.dumps(event, indent=2))
             reportingMTA = "UNKNOWN"
-            
+
         bounceType = SESjson['bounce']['bounceType']
         bounceRecipients = SESjson['bounce']['bouncedRecipients']
         bounceType = SESjson['bounce']['bounceType']
         bounceSubType = SESjson['bounce']['bounceSubType']
         bounceTimestamp = SESjson['bounce']['timestamp'] # the time at which the bounce was sent by the ISP
-        
+
         # There can be a seperate bounce reason per recipient IF it's not a suppression bounce
         for recipient in bounceRecipients:
             try:
@@ -65,22 +67,26 @@ def lambda_handler(event, context):
                 print "No recipient email provided in bounce notification"
                 print("Received event: " + json.dumps(event, indent=2))
                 recipientEmailAddress = "UNKNOWN"
-            
+
             try:
                 diagnosticCode = recipient['diagnosticCode']
             except:
                 print "No diagnosticCode provided in bounce notification"
                 print("Received event: " + json.dumps(event, indent=2))
                 diagnosticCode = "UNKNOWN"
-            
+
             print("Bounced recipient: " + str(recipientEmailAddress) + " reason: " + str(diagnosticCode))
-            
+
             sesTimestamp_parsed = dateutil.parser.parse(sesTimestamp)
             sesTimestamp_seconds = sesTimestamp_parsed.strftime('%s')
 
             bounceTimestamp_parsed = dateutil.parser.parse(bounceTimestamp)
             bounceTimestamp_seconds = bounceTimestamp_parsed.strftime('%s')
-            
+
+            # Set an expiry time for this record so we can use Dynamo TTLs to remove
+            future = datetime.datetime.utcnow() + datetime.timedelta(days=120)
+            expiry_ttl = calendar.timegm(future.timetuple())
+
             # Add entry to DB for this recipient
             Item={
                 'recipientAddress': recipientEmailAddress,
@@ -91,16 +97,17 @@ def lambda_handler(event, context):
                 'diagnosticCode': diagnosticCode,
                 'bounceType': bounceType,
                 'bounceSubType': bounceSubType,
-                'sender': sender.lower()
+                'sender': sender.lower(),
+                'expiry': long(expiry_ttl)
             }
-            
+
             response = DDBtable.put_item(Item=Item)
             print("PutItem succeeded:")
             print(json.dumps(response, indent=4, cls=DecimalEncoder))
-            
+
             processed = True
-        
+
     else:
-        print("Unhandled notification type: " +  sesNotificationType)       
-    
+        print("Unhandled notification type: " +  sesNotificationType)
+
     return processed
--- lambda-functions/storeSESDelivery/src/storeSESDelivery_lambda.py
+++ lambda-functions/storeSESDelivery/src/storeSESDelivery_lambda.py
@@ -2,6 +2,8 @@ import boto3
 import json
 import time
 import dateutil.parser
+import datetime
+import calendar
 
 # Helper class to convert a DynamoDB item to JSON.
 class DecimalEncoder(json.JSONEncoder):
@@ -15,53 +17,58 @@ class DecimalEncoder(json.JSONEncoder):
 
 def lambda_handler(event, context):
     #print("Received event: " + json.dumps(event, indent=2))
-    
+
     processed = False
     DYNAMODB_TABLE = "DEVOPS_SES_DELIVERIES"
-    
+
     DDBtable = boto3.resource('dynamodb').Table(DYNAMODB_TABLE)
-    
+
     # Generic SNS headers
     SnsMessageId = event['Records'][0]['Sns']['MessageId']
     SnsPublishTime = event['Records'][0]['Sns']['Timestamp']
     SnsTopicArn = event['Records'][0]['Sns']['TopicArn']
     SnsMessage = event['Records'][0]['Sns']['Message']
-    
+
     print("Read SNS Message with ID " + SnsMessageId + " published at " + SnsPublishTime)
-    
+
     now = time.strftime("%c")
     LambdaReceiveTime = now;
-    
+
     # SES specific fields
     SESjson = json.loads(SnsMessage)
     sesNotificationType = SESjson['notificationType']
     sesMessageId = SESjson['mail']['messageId']
     sesTimestamp = SESjson['mail']['timestamp']
     sender = SESjson['mail']['source']
-    
+
     print("Processing an SES " + sesNotificationType + " with mID " + sesMessageId )
-    
+
     if (sesNotificationType == "Delivery"):
         print "Processing SES delivery messsage"
-        
+
         reportingMTA = SESjson['delivery']['reportingMTA']
         deliveryRecipients = SESjson['delivery']['recipients']
         smtpResponse = SESjson['delivery']['smtpResponse']
         deliveryTimestamp = SESjson['delivery']['timestamp']
         processingTime = SESjson['delivery']['processingTimeMillis']
-        
+
         # there can be multiple recipients but the SMTPresponse is the same for each
         for recipient in deliveryRecipients:
             recipientEmailAddress = recipient
-            
+
             print("Delivery recipient: " + recipientEmailAddress )
-            
+
             sesTimestamp_parsed = dateutil.parser.parse(sesTimestamp)
             sesTimestamp_seconds = sesTimestamp_parsed.strftime('%s')
-            
+
             deliveryTimestamp_parsed = dateutil.parser.parse(deliveryTimestamp)
             deliveryTimestamp_seconds = deliveryTimestamp_parsed.strftime('%s')
-            
+
+            # Set an expiry time for this record so we can use Dynamo TTLs to remove
+            # 4 months but easy to change
+            future = datetime.datetime.utcnow() + datetime.timedelta(days=120)
+            expiry_ttl = calendar.timegm(future.timetuple())
+
             # Add entry to DB for this recipient
             Item={
                 'recipientAddress': recipientEmailAddress,
@@ -71,16 +78,17 @@ def lambda_handler(event, context):
                 'processingTime': long(processingTime),
                 'reportingMTA': reportingMTA,
                 'smtpResponse': smtpResponse,
-                'sender': sender.lower()
+                'sender': sender.lower(),
+                'expiry': long(expiry_ttl)
             }
-            
+
             response = DDBtable.put_item(Item=Item)
             print("PutItem succeeded:")
             print(json.dumps(response, indent=4, cls=DecimalEncoder))
-            
+
             processed = True
-        
+
     else:
-        print("Unhandled notification type: " +  sesNotificationType)       
-    
+        print("Unhandled notification type: " +  sesNotificationType)
+
     return processed
